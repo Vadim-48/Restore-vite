790ab5209fcc81031291171c292a69b5ea29a282
Vadim-48 <rvadim4835@gmail.com>
2025-11-27 17:35:35 +0200

    Updates

---
+++ assets/header-scroll-CgmVNZ9u.js
@@ -0,0 +1,1 @@
+function l(){window.addEventListener("scroll",()=>{const e=document.getElementById("header__wrap");e&&e.classList.toggle("scrolled",window.scrollY>0)})}export{l as h};
---
+++ assets/solutions-OLuUmw7I.js
@@ -0,0 +1,1 @@
+import{b as e}from"./burger-CDZVLKof.js";import{h as r}from"./header-scroll-CgmVNZ9u.js";document.addEventListener("DOMContentLoaded",()=>{e(),r()});
